bf2d0585c105d3619b0080b30176662e6d10cfe2
LuizaBB <luizabezerrabastos@gmail.com>
2024-07-24 15:22:49 -0300

    Update Teste 1 Semana 1.py

--- Teste 1 Semana 1.py
+++ Teste 1 Semana 1.py
@@ -7,7 +7,7 @@ def create_account():
         user_name=str(input("Insert your name: "))
         user_age=int(input("Insert your age: "))
         user_password=str(input("Insert your password (8 carater max.): "))
-        account_information=[f"Name: {user_name}", f"\nAge: {user_age}", F"\nPassword: {user_password}\n\n"]
+        account_information=[f"Name: {user_name}", f"\nAge: {user_age}", f"\nPassword: {user_password}\n\n"]
         print(f"Collected information: {account_information}")
         database.writelines(account_information)
         database.close()
@@ -16,11 +16,9 @@ def user_main_option(user_option):
     match user_option:
             case 0:
                 create_account()
-            # case 1:
-            #     login()
             case 1:
                 educational_resources()
 def main():
-        main_option=int(input("0: Create Account\n1: Login\n"))
+        main_option=int(input("Welcome:\n0: Create Account\n1: Educational Information\nOption: "))
         user_main_option(main_option)
 main()
