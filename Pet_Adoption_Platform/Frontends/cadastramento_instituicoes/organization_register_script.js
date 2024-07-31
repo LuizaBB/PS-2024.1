cc3a5a7996c2505adf43dddbce40f9755df0ef2c
LuizaBB <luizabezerrabastos@gmail.com>
2024-07-31 01:05:00 -0300

    input radio no cadastro de instituições
    adicionado radio junto com modificações na função de leitura dos inputs (no momento funciona bem apenas quando todos os inputs estão preenchidos) -> colocar obrigatoriedades(requests)

--- Pet_Adoption_Platform/Frontends/cadastramento_instituicoes/organization_register_script.js
+++ Pet_Adoption_Platform/Frontends/cadastramento_instituicoes/organization_register_script.js
@@ -3,15 +3,15 @@ function print(information){
 }
 
 function read_register_informations(){
-    let organization_name = document.getElementById("organization_name").value;
-    let organization_email = document.getElementById("organization_email").value;
-    let organization_password = document.getElementById("organization_password").value;
-    let organization_focus=document.getElementById("organization_focus").value;
-    let organization_description = document.getElementById("organization_description").value;
+    const name = document.getElementById("organization_name").value;
+    const email = document.getElementById("organization_email").value;
+    const password = document.getElementById("organization_password").value;
+    const focus=document.querySelector('input[name="focus"]:checked').value;
+    const description = document.getElementById("organization_description").value;
 
-    let register_information=[organization_name, organization_email, organization_password, organization_focus, organization_description];
-    return register_information;
+    return [name, email, password, focus, description];
 }
+
 function main(){
     let info=read_register_informations();
     print(info);
