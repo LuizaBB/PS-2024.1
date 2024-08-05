191d71eb0f1ffcea67800ec9f0e5f2ed9a051f1c
LuizaBB <luizabezerrabastos@gmail.com>
2024-08-05 00:07:29 -0300

    ajuste para conectar códigos html
    Linkagem entre códigos e formularios para login e cadastro de instituições

--- Pet_Adoption_Platform/plataform_flask.py
+++ Pet_Adoption_Platform/plataform_flask.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import Institution_Registration_Form, Login_Form
 web=Flask(__name__)
 
-@web.route("/")
-@web.route("/home")
-def render_home():
-    return render_template("/pagina_principal_wtlogin/mainwt_webpage.html")
+web.config['SECRET_KEY']='123456789'
 
-@web.route("/login")
+@web.route("/")
+def render_home():
+    return render_template("main.html")
+#criar pagina principal com login e colocar aqui
+@web.route("/login", methods=['GET', 'POST'])
 def render_login():
-    return render_template("/pagina_login/login_webpage.html")
+    login_form=Login_Form()
+    if login_form.validate_on_submit():
+        flash(f"Conta verificada para {login_form.email.data}", "sucess")
+        return redirect(url_for('render_home'))
+    return render_template("login.html", forms=login_form,title="Login")
+
+@web.route("/main_register")
+def render_main_register():
+    return render_template("main_register_webpage.html")
+
+@web.route("/institucion_register", methods=['GET', 'POST'])
+def render_institucion_register():
+    institucion_form= Institution_Registration_Form()
+    if institucion_form.validate_on_submit():
+        flash(f"Conta criada para {institucion_form.name.data}", "sucess")
+        return redirect(url_for('render_home'))
+    return render_template("organization_register.html", title="Organization", forms=institucion_form)
+
 if __name__=="__main__":
     web.run(debug=True)
